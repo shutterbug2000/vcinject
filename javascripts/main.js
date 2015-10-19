a8b49defbf955d26e8db3c2762904d026b91d0e9
shutterbug2000 <shutterbug20002@yahoo.com>
2015-10-19 01:50:12 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime){
   var timeinterval = setInterval(updateClock,1000);
 }
 
-var deadline = 'October 19 2015 00:00:00 UTC-0600';
+var deadline = 'October 20 2015 18:00:00 UTC-0600';
 initializeClock('clockdiv', deadline);
